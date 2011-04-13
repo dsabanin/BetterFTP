496ff56c33ec2a0fe18c25bff0f5aeb4696e4e61
Dima Sabanin <sdmitry@gmail.com>
2011-04-13 18:12:16 +0800

    First version, just wrote it today, expect this to mature

---
+++ better_ftp.rb
@@ -0,0 +1,96 @@
+require 'net/ftp'
+
+class BetterFTP < Net::FTP
+
+  attr_accessor :port
+  alias_method  :cd, :chdir
+  
+  def initialize(host = nil, user = nil, passwd = nil, acct = nil)
+    super
+    @host = host
+    @user = user
+    @passwd = passwd
+    @acct = acct
+    @created_paths_cache = []
+  end
+  
+  def connect(host, port = nil)
+    port ||= @port || FTP_PORT
+    if @debug_mode
+      print "connect: ", host, ", ", port, "\n"
+    end
+    synchronize do
+      @created_paths_cache = []      
+      @sock = open_socket(host, port)
+      voidresp
+    end
+  end
+
+  def reconnect!
+    if @host
+      connect(@host)
+      if @user
+        login(@user, @passwd, @acct)
+      end
+    end
+  end
+  
+  def directory?(path)
+    chdir(path)
+    
+    return true
+  rescue Net::FTPPermError
+    return false
+  end
+  
+  def file?(path)
+    chdir(File.dirname(path))
+
+    begin
+      size(path)
+      return true
+    rescue Net::FTPPermError
+      return false
+    end
+  end
+  
+  def mkdir_p(dir)
+    parts = dir.split("/")
+    growing_path = "/"
+    for part in parts
+      next if part == ""
+      growing_path = File.join(growing_path, part)
+      unless @created_paths_cache.include?(growing_path)
+        puts "Creating #{growing_path.inspect}" if @debug_mode
+        begin
+          mkdir(growing_path)
+          chdir(growing_path)
+          @created_paths_cache << growing_path
+        rescue Net::FTPPermError => e
+          puts "Received #{e.class}: #{e.message}" if @debug_mode
+        end
+      else
+        puts "Cache says we already created #{growing_path.inspect}" if @debug_mode
+      end
+    end
+  end
+  
+  def rm_r(path)
+    if directory?(path) 
+      chdir path
+
+      files = nlst
+      files.each {|file| rm_r "#{path}/#{file}"}
+
+      rmdir path
+    else
+      rm(path)
+    end
+  end
+  
+  def rm(path)
+    chdir File.dirname(path)
+    delete File.basename(path)
+  end
+  
+end
